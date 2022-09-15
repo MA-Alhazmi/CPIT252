b9bcaadb67a82553caae0f19dc4d34917b96187a
M.Alhazmi <113578474+MA-Alhazmi@users.noreply.github.com>
2022-09-15 10:52:15 +0300

    Add files via upload

---
+++ lab2/src/Logger.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class Logger {
+    DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+    String currentDateTimeString = LocalDateTime.now().format(myFormatter);
+    private final String logFile = currentDateTimeString + ".txt";
+    private PrintWriter writer;
+    private static Logger uniqueInstance;
+
+    private Logger() {
+        try {
+            FileWriter fw = new FileWriter(logFile);
+            writer = new PrintWriter(fw, true);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static Logger getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Logger();
+        }
+        return uniqueInstance;
+    }
+
+    public void info(String message) {
+        writer.println("INFO: " + message);
+    }
+
+    public void error(String message) {
+        writer.println("Error: " + message);
+    }
+}
